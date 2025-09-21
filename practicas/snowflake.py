fdeb263a38462e7718c2d76d041284043c14ea1b
Ronny Olivares <ryogindieinc@gmail.com>
2025-09-20 22:54:06 -0500

    feat(snowflake): imprimir copo de nieve en ASCII con narrativa RPG

---
+++ practicas/snowflake.py
@@ -0,0 +1,25 @@
+# src/snowflake.py
+
+"""
+Practicar strings y cómo imprimir texto en diferentes formas.
+
+Usar print() para crear un diseño visual.
+
+Reforzar la idea de que un programa no solo calcula cosas, también puede mostrar arte ASCII.
+
+Contexto narrativo (RPG vibes ❄️)
+
+Tu héroe llega a una montaña helada. De pronto, la nieve empieza a caer y aparece un mágico copo de nieve en el aire.
+El programa debe mostrar ese copo en pantalla usando caracteres.
+"""
+
+line1 = "  *  "
+line2 = " *** "
+line3 = "*****"
+
+print(f"🌨️ Un copo de nieve aparece en el aire...")
+print(f"\t {line1}")
+print(f"\t {line2}")
+print(f"\t {line3}")
+print(f"\t {line2}")
+print(f"\t {line1}")
